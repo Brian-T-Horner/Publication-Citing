7d05754d6ba43a51c228c3283b8332e32d3c5e1b
Brian Horner <bhorner@suffolk.edu>
2023-01-08 18:59:40 -0500

    Added comments for unit tests

--- src/PublicationTest.java
+++ src/PublicationTest.java
@@ -13,12 +13,18 @@ public class PublicationTest {
   private Publication turing;
   private Publication ccis;
 
+  /**
+   * Initializes a new book for testing MLA and APA citation styles
+   */
   @Before
   public void setupBook(){
     rushdie = new Book("Midnight's Children", "Salman Rushdie", "Jonathan Cape",
         "London", 1980);
   }
 
+  /**
+   * Initializes a new article for testing MLA and APA citation styles
+   */
   @Before
   public void setupArticle(){
    turing = new Article("Computing machinery and intelligence", "A. M. Turing",
@@ -27,6 +33,9 @@ public class PublicationTest {
 
 
 
+  /**
+   * Initializes a new webpage for testing MLA and APA citation styles
+   */
   @Before
   public void setupWebpage(){
     ccis = new Webpage("CCIS at Northeastern University",
@@ -34,30 +43,45 @@ public class PublicationTest {
   }
 
 
+  /**
+   * Tests the book APA citation method.
+   */
   @Test
   public void testCiteApaBook(){
     String expectedOutput = "Salman Rushdie (1980). Midnight's Children. London: Jonathan Cape.";
     assertEquals(expectedOutput, rushdie.citeApa());
   }
 
+  /**
+   * Tests the book MLA citation method.
+   */
   @Test
   public void testCiteMlaBook(){
     String expectedOutput = "Salman Rushdie. Midnight's Children. London: Jonathan Cape, 1980.";
     assertEquals(expectedOutput, rushdie.citeMla());
   }
 
+  /**
+   * Tests the article APA citation method.
+   */
   @Test
   public void testCiteApaArticle(){
     String expectedOutput = "A. M. Turing (1950). Computing machinery and intelligence. Mind, 59(236).";
     assertEquals(expectedOutput, turing.citeApa());
   }
 
+  /**
+   * Tests the article MLA citation method.
+   */
   @Test
   public void testCiteMlaArticle(){
     String expectedOutput = "A. M. Turing. \"Computing machinery and intelligence.\" Mind 59.236 (1950).";
     assertEquals(expectedOutput, turing.citeMla());
   }
 
+  /**
+   * Tests the webpage APA citation method.
+   */
   @Test
   public void testCiteApaWebpage(){
     String expectedOutput = "CCIS at Northeastern University. Retrieved 10th August 2018, from https://www.ccis"
@@ -65,6 +89,9 @@ public class PublicationTest {
     assertEquals(expectedOutput, ccis.citeApa());
   }
 
+  /**
+   * Tests the webpage MLA citation method.
+   */
   @Test
   public void testCiteMlaWebpage(){
     String expectedOutput = "\"CCIS at Northeastern University.\" Web. 10th August 2018 "
